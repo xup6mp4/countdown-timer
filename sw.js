d3f2009f6f7487f6e11511c8b05c70769a7b4bcb
xup6mp4 <100010394+xup6mp4@users.noreply.github.com>
2025-10-24 01:11:48 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+// sw.js v4
+const CACHE = 'countdown-timer-v4';
+const ASSETS = [
+  './',
+  './index.html?v=4',
+  './manifest.webmanifest?v=4',
+  './icons/icon-180.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request, { ignoreSearch: false }).then(r => r || fetch(e.request)));
+});
